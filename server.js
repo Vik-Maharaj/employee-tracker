bf0987519fc8c41c83cdb4454586dd03f917322e
Vikaash Maharaj <vikaashmaharaj@gmail.com>
2022-05-14 15:54:28 -0400

    Add server functions

--- server.js
+++ server.js
@@ -19,3 +19,38 @@ app.use('/api', apiRoutes);
 app.use((req, res) => {
     res.status(404).end();
 });
+
+db.connect(err => {
+    if (err) throw err;
+    console.log('Database connected.');
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
+    });
+});
+
+function viewEmployees() {
+    var query = 'SELECT * FROM employee';
+    db.query(query, function(err, res) {
+        if (err) throw err;
+        console.table('EMPLOYEES', res);
+        promptQuestions();
+    })
+};
+
+function viewRoles() {
+    var query = 'SELECT * FROM role';
+    db.query(query, function(err, res) {
+        if (err) throw err;
+        console.table('ROLES', res);
+        promptQuestions();
+    })
+};
+
+function viewDepartments() {
+    var query = 'SELECT * FROM department';
+    db.query(query, function(err, res) {
+        if (err) throw err;
+        console.table('DEPARTMENTS', res);
+        promptQuestions();
+    })
+};
