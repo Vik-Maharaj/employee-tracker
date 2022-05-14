7c30e417dac779d53ffe1ff16e4760c979954e0b
Vikaash Maharaj <vikaashmaharaj@gmail.com>
2022-05-14 17:33:47 -0400

    Fix employee/department function

--- server.js
+++ server.js
@@ -56,10 +56,15 @@ function viewDepartments() {
 
 
 function employeeDepartment() {
-    var query = 'SELECT * FROM department';
+   var query =   `SELECT employee.first_name, 
+                  employee.last_name, 
+                  department.name AS department
+                  FROM employee 
+                  LEFT JOIN role ON employee.role_id = role.id 
+                  LEFT JOIN department ON role.department_id = department.id`;
     db.query(query, function(err, res) {
         if (err) throw err;
-        console.table('DEPARTMENTS', res);
+        console.table('EMPLOYEE/DEPARTMENTS', res);
         userQuestions();
     })
 };
