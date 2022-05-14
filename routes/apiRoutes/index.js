59cd78bea1b4575085f0c62d82ea9955336cf406
Vikaash Maharaj <vikaashmaharaj@gmail.com>
2022-05-14 13:50:45 -0400

    Add department routes

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
+
 const router = express.Router();
 
 router.use(require('./employeeRoutes'));
-router.use(require('./departmentRoutes'));
 router.use(require('./roleRoutes'));
+router.use(require('./departmentRoutes'));
+
 
 module.exports = router;
